9294cf4600cb81b7b038c80accfce22c814fb7ea
chandadeep1-cmyk <chandadeep1@gmail.com>
2026-09-17 22:36:51 +0530

    Create display_app.py

---
+++ display_app.py
@@ -0,0 +1,24 @@
+import gradio as gr
+import pickle
+
+# Load model
+model = pickle.load(open("student_model.pkl", "rb"))
+
+# Prediction function
+def predict(hours):
+    prediction = model.predict([[hours]])
+    
+    if prediction[0] == 1:
+        return "PASS ✅"
+    else:
+        return "FAIL ❌"
+
+# Create Gradio app
+app = gr.Interface(
+    fn=predict,
+    inputs="number",
+    outputs="text",
+    title="Study Hours - Pass/Fail Predictor"
+)
+
+app.launch()
